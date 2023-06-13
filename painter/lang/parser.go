acdd34b1a6916a2450af30ac418e42d9275a64b7
mikhmol <mikhailo.moliakov@gmail.com>
2023-06-14 00:00:47 +0300

    Added parser code

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -1,21 +1,119 @@
 package lang
 
 import (
+	"bufio"
+	"errors"
 	"io"
+	"strconv"
+	"strings"
 
 	"github.com/mikhmol/Architecture_Lab3/painter"
 )
 
 // Parser уміє прочитати дані з вхідного io.Reader та повернути список операцій представлені вхідним скриптом.
 type Parser struct {
+	bgRect  *painter.BgRectangle
+	backOp  painter.Operation
+	move    []painter.Operation
+	figures []*painter.Figure
+	res     []painter.Operation
+	update  painter.Operation
+	updated bool
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
+	p.update = nil
+	p.res = nil
+	if p.backOp == nil {
+		p.backOp = painter.OperationFunc(painter.WhiteFill)
+	}
+
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		commandLine := scanner.Text()
+		if len(commandLine) == 0 {
+			continue
+		}
+		err := p.parse(commandLine) // parse the line to get Operation
+		if err != nil {
+			return nil, err
+		}
+		if p.updated {
+			p.updated = false
+			p.res = p.genOperations()
+		}
+	}
+
+	return p.res, scanner.Err()
+}
+
+func (p *Parser) genOperations() []painter.Operation {
 	var res []painter.Operation
 
-	// TODO: Реалізувати парсинг команд.
-	res = append(res, painter.OperationFunc(painter.WhiteFill))
-	res = append(res, painter.UpdateOp)
+	if p.backOp != nil {
+		res = append(res, p.backOp)
+	}
 
-	return res, nil
+	if p.bgRect != nil {
+		res = append(res, p.bgRect)
+	}
+
+	if p.move != nil {
+		res = append(res, p.move...)
+		p.move = nil
+	}
+
+	for _, figure := range p.figures {
+		res = append(res, figure)
+	}
+
+	if p.update != nil {
+		res = append(res, p.update)
+	}
+
+	return res
+}
+
+func (p *Parser) parse(commandLine string) error {
+	fields := strings.Fields(commandLine)
+	operation := fields[0]
+	var args []int
+
+	for i := 1; i < len(fields); i++ {
+		arg, err := strconv.ParseFloat(fields[i], 64)
+		if err != nil {
+			return err
+		}
+		if arg > 0 && arg < 1 {
+			arg = arg * 800.0
+		}
+		args = append(args, int(arg))
+	}
+	switch operation {
+	case "white":
+		p.backOp = painter.OperationFunc(painter.WhiteFill)
+	case "green":
+		p.backOp = painter.OperationFunc(painter.GreenFill)
+	case "update":
+		p.updated = !p.updated
+		p.update = painter.UpdateOp
+	case "bgrect":
+		p.bgRect = &painter.BgRectangle{X1: args[0], Y1: args[1], X2: args[2], Y2: args[3]}
+	case "figure":
+		figure := &painter.Figure{X: args[0], Y: args[1]}
+		p.figures = append(p.figures, figure)
+	case "move":
+		moveOp := &painter.Move{X: args[0], Y: args[1], Figures: p.figures}
+		p.move = append(p.move, moveOp)
+	case "reset":
+		p.figures = nil
+		p.bgRect = nil
+		p.move = nil
+		p.backOp = painter.OperationFunc(painter.ResetScreen)
+	default:
+		return errors.New("Failed")
+	}
+	return nil
 }
